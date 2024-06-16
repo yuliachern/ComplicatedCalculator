c62af8f8e77679093ec981a4f1044d90d4c8bdba
yuliach <yuliachern9@gmail.com>
2024-06-16 13:19:04 +0200

    added 5 var

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -1,3 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Half
+﻿// Declare variables
+int num1 = 0;
+int num2 = 0;
+
+// Prompt the user for input
+
+Console.WriteLine("Please enter first number");
+num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please enter second number");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+//Process the data
+int addition = num1 + num2;
+int multiplication = num1 * num2;
+int devision = num1 / num2;
+int subtraction = num1 - num2;
+int modulus = num1 % num2;
+
+//Output the result to the user
+Console.WriteLine($"The result of addition is {addition}");
+Console.WriteLine($"The result of multiplication is {multiplication}");
+Console.WriteLine($"The result of devision is {devision}");
+Console.WriteLine($"The result of subtraction is {subtraction}");
+Console.WriteLine($"The result of modulus is {modulus}");
+
+
