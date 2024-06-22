4807fd5595cb4fb43177466b925f7aab767e7e49
yuliach <yuliachern9@gmail.com>
2024-06-22 10:49:14 +0200

    try, catch, finally blocks

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -19,99 +19,123 @@ num2 = Convert.ToInt32(Console.ReadLine());
 
 
 //Process the data
-int addition = num1 + num2;
-int multiplication = num1 * num2;
-int devision = num1 / num2;
-int subtraction = num1 - num2;
-int modulus = num1 % num2;
+//int addition = num1 + num2;
+//int multiplication = num1 * num2;
+//int devision = num1 / num2;
+//int subtraction = num1 - num2;
+//int modulus = num1 % num2;
 
 //Output the result to the user
-Console.WriteLine($"The result of addition is {addition}");
-Console.WriteLine($"The result of multiplication is {multiplication}");
-Console.WriteLine($"The result of devision is {devision}");
-Console.WriteLine($"The result of subtraction is {subtraction}");
-Console.WriteLine($"The result of modulus is {modulus}");
+//Console.WriteLine($"The result of addition is {addition}");
+//Console.WriteLine($"The result of multiplication is {multiplication}");
+//Console.WriteLine($"The result of devision is {devision}");
+//Console.WriteLine($"The result of subtraction is {subtraction}");
+//Console.WriteLine($"The result of modulus is {modulus}");
 int choice = 0;
 while (choice != -1)
 {
-   
-    //Show calculator options / Show menu
-    Console.WriteLine("Please select en operation (-1 to exit program)");
-    Console.WriteLine("1 -  addition");
-    Console.WriteLine("2 - multiplication");
-    Console.WriteLine("3 - devisio");
-    Console.WriteLine("4 - subtraction");
-    Console.WriteLine("5 - modulus");
-    Console.WriteLine("6- Fibonacci sequence");
-
     try
     {
-        choice = Convert.ToInt32(Console.ReadLine());
+
+        //Show calculator options / Show menu
+        Console.WriteLine("Please select en operation (-1 to exit program)");
+        Console.WriteLine("1 -  addition");
+        Console.WriteLine("2 - multiplication");
+        Console.WriteLine("3 - devisio");
+        Console.WriteLine("4 - subtraction");
+        Console.WriteLine("5 - modulus");
+        Console.WriteLine("6- Fibonacci sequence");
+
+        try
+        {
+            choice = Convert.ToInt32(Console.ReadLine());
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Invalid input, press any key to try again");
+            Console.ReadLine();
+            Console.Clear();
+            // throw;
+            continue;
+        }
+
+
+        if (choice == -1)
+        {
+            break;
+        }
+
+
+        try
+        {
+            Console.WriteLine("Please enter first number");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter second number");
+            num2 = Convert.ToInt32(Console.ReadLine());
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Invalid input, press any key to try again");
+            Console.ReadLine();
+            Console.Clear();
+            // throw;
+            continue;
+        }
+
+
+
+        int answer = 0;
+        switch (choice)
+        {
+            case 1:
+                answer = num1 + num2;
+                break;
+            case 2:
+                answer = num1 - num2;
+                break;
+            case 3:
+                answer = num1 * num2;
+                break;
+            case 4:
+                try
+                {
+                    answer = num1 / num2;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Cannot divide by zero");
+                    Console.ReadLine();
+                    Console.Clear();
+                    // throw;
+                    continue;
+                }
+
+                break;
+            case 5:
+                answer = num1 % num2;
+                break;
+            case 6:
+                for (int i = num1; i <= num2; i++)
+                {
+                    answer += i;
+                }
+                break;
+            default:
+                throw new Exception("Invalid menu item selected.");
+                Console.WriteLine("Invalid choice");
+                break;
+        }
+        Console.WriteLine($"The result is {answer}");
     }
-    catch (Exception)
+    catch(Exception ex)
     {
         Console.WriteLine("Invalid input, press any key to try again");
         Console.ReadLine();
-        Console.Clear();
-       // throw;
-       continue;
-    }
-
-      
-    if (choice == -1)
-    {
-        break;
-    }
-
-
-    try
-    {
-        Console.WriteLine("Please enter first number");
-        num1 = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Please enter second number");
-        num2 = Convert.ToInt32(Console.ReadLine());
-    }
-    catch (Exception)
-    {
-        Console.WriteLine("Invalid input, press any key to try again");
-        Console.ReadLine();
+        Console.WriteLine(ex.Message);
         Console.Clear();
         // throw;
         continue;
     }
-
-
-
-    int answer = 0;
-    switch (choice)
-    {
-        case 1:
-            answer = num1 + num2;
-            break;
-        case 2:
-            answer = num1 - num2;
-            break;
-        case 3:
-            answer = num1 * num2;
-            break;
-        case 4:
-            answer = num1 / num2;
-            break;
-        case 5:
-            answer = num1 % num2;
-            break;
-        case 6:
-            for (int i = num1; i <= num2; i++)
-            {
-                answer += i;
-            }
-            break;
-        default:
-            Console.WriteLine("Invalid choice");
-            break;
-    }
-    Console.WriteLine($"The result is {answer}");
-    
     
 
 }
