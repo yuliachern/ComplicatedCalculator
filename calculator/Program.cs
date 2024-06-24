e5e6d3d26d3e844673d35d7d0eb0f916dc3ba2f7
yuliach <yuliachern9@gmail.com>
2024-06-24 10:01:00 +0200

    added methods

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -6,17 +6,15 @@ int num1 = 0;
 int num2 = 0;
 
 // Prompt user for input
-
 Console.WriteLine("Please enter first number");
 num1 = Convert.ToInt32(Console.ReadLine());
+
 Console.WriteLine("Please enter second number");
 num2 = Convert.ToInt32(Console.ReadLine());
 
 
 
 //Decide wich operation is needed  based on selected option
-
-
 
 //Process the data
 //int addition = num1 + num2;
@@ -34,91 +32,43 @@ num2 = Convert.ToInt32(Console.ReadLine());
 int choice = 0;
 while (choice != -1)
 {
+    //Show calculator options / Show menu
+    PrintMenue();
+
     try
     {
-
-        //Show calculator options / Show menu
-        Console.WriteLine("Please select en operation (-1 to exit program)");
-        Console.WriteLine("1 -  addition");
-        Console.WriteLine("2 - multiplication");
-        Console.WriteLine("3 - devisio");
-        Console.WriteLine("4 - subtraction");
-        Console.WriteLine("5 - modulus");
-        Console.WriteLine("6- Fibonacci sequence");
-
-        try
-        {
-            choice = Convert.ToInt32(Console.ReadLine());
-        }
-        catch (Exception)
-        {
-            Console.WriteLine("Invalid input, press any key to try again");
-            Console.ReadLine();
-            Console.Clear();
-            // throw;
-            continue;
-        }
-
-
+        choice = Convert.ToInt32(Console.ReadLine());
         if (choice == -1)
         {
             break;
         }
+        Console.WriteLine("Please enter first number");
+        num1 = Convert.ToInt32(Console.ReadLine());
 
-
-        try
-        {
-            Console.WriteLine("Please enter first number");
-            num1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Please enter second number");
-            num2 = Convert.ToInt32(Console.ReadLine());
-        }
-        catch (Exception)
-        {
-            Console.WriteLine("Invalid input, press any key to try again");
-            Console.ReadLine();
-            Console.Clear();
-            // throw;
-            continue;
-        }
-
+        Console.WriteLine("Please enter second number");
+        num2 = Convert.ToInt32(Console.ReadLine());
 
 
         int answer = 0;
         switch (choice)
         {
             case 1:
-                answer = num1 + num2;
+                answer = addition(num1, num2);
                 break;
             case 2:
-                answer = num1 - num2;
+                answer = subtraction(num1, num2);
                 break;
             case 3:
-                answer = num1 * num2;
+                answer = multiplication(num1,num2);
                 break;
             case 4:
-                try
-                {
-                    answer = num1 / num2;
-                }
-                catch (Exception)
-                {
-                    Console.WriteLine("Cannot divide by zero");
-                    Console.ReadLine();
-                    Console.Clear();
-                    // throw;
-                    continue;
-                }
-
-                break;
+                 answer = devision(num1, num2);
+                 break;
             case 5:
-                answer = num1 % num2;
+                answer = modulus(num1, num2);
                 break;
             case 6:
-                for (int i = num1; i <= num2; i++)
-                {
-                    answer += i;
-                }
+                answer = Fibonacci(num1, num2);
                 break;
             default:
                 throw new Exception("Invalid menu item selected.");
@@ -127,15 +77,62 @@ while (choice != -1)
         }
         Console.WriteLine($"The result is {answer}");
     }
-    catch(Exception ex)
+
+    catch (DivideByZeroException)
+    {
+        Console.WriteLine("Can not divide by zero");
+    }
+
+    catch (Exception ex)
     {
         Console.WriteLine("Invalid input, press any key to try again");
+    }
+    finally
+    {
         Console.ReadLine();
-        Console.WriteLine(ex.Message);
         Console.Clear();
-        // throw;
-        continue;
     }
     
+}
 
+void PrintMenue()
+{
+    Console.WriteLine("Please select en operation (-1 to exit program)");
+    Console.WriteLine("1 -  addition");
+    Console.WriteLine("2 - multiplication");
+    Console.WriteLine("3 - devisio");
+    Console.WriteLine("4 - subtraction");
+    Console.WriteLine("5 - modulus");
+    Console.WriteLine("6- Fibonacci sequence");
+}
+
+// Methods
+int addition(int num1, int num2)
+{
+    return num1 + num2;
+}
+int multiplication(int num1, int num2)
+{
+    return num1 * num2;
+}
+int devision(int num1, int num2)
+{
+    return num1 / num2;
+}
+int subtraction(int num1, int num2)
+{
+    return num1 - num2;
+}
+int modulus(int num1, int num2)
+{
+    return num1 % num2;
+}
+int Fibonacci(int num1, int num2)
+{
+    int answer = 0;
+    for (int i = num1; i <= num2; i++)
+    {
+        answer += i;
+    }
+    return answer;
 }
